00b501dcc773da41db60d44fa06ad19e4cccfdc5
TheWaffleLord1208 <agentsans8@gmail.com>
2025-09-10 13:43:16 -0700

    player movement?

---
+++ Unity-Solo/Assets/scripts/player movement.cs
@@ -0,0 +1,35 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    float inputX;
+    float inputY;
+
+    public float speed = 5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+
+        Vector3 tempMove = rb.linearVelocity;
+        tempMove.x = inputY * speed;
+        tempMove.z = inputX * speed;
+
+        rb.linearVelocity = (tempMove.x * transform.forward) + (tempMove.y * transform.up) + (tempMove.z * transform.right);
+     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 InputAxis = context.ReadValue<Vector2>();
+
+        inputX = InputAxis.x;
+        inputY = InputAxis.y;
+    }
+
+}
