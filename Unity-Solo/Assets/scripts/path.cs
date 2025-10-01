d935f578b5ed6e3f1ef14b1984294d80067ef9bd
TheWaffleLord1208 <agentsans8@gmail.com>
2025-10-01 13:40:31 -0700

    Work on enemy path system

--- Unity-Solo/Assets/scripts/path.cs
+++ Unity-Solo/Assets/scripts/path.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 public class path : MonoBehaviour
 {
+    public Vector3 startPosition;
     public Vector3 targetPosition;
     public float speed = 5f;
     void Start()
     {
-        
+
     }
     void Update()
     {
-       transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed); 
+        if (currentPosition != targetPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, speed * Time.deltaTime);
+        }
     }
+
 }
